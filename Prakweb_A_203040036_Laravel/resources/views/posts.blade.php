ff914ee38eab9e40bb9ba4bb4a5b4ef6a93642bd
caprio <203040036@mail.ac.id>
2022-10-17 23:03:57 +0700

    Menyelesaikan tugas Laravel

---
+++ Prakweb_A_203040036_Laravel/resources/views/posts.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('container')
+    @foreach ($posts as $posts)
+    <article class="mb-5">
+        <h2>
+            <a href="/posts/{{ $posts["slug"] }}">{{ $posts["tittle"] }}</a>
+        </h2>
+        <h5>By: {{ $posts["author"] }}</h5>
+        <p>{{ $posts["body"] }}</p>
+    </article>   
+    @endforeach
+
+@endsection
