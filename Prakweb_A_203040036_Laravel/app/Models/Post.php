d59246bc3af0ab9cd1956d2a6da7b5e3275a5002
caprio <203040036@mail.ac.id>
2022-10-27 00:34:27 +0700

    menyelesaikan video 9

--- Prakweb_A_203040036_Laravel/app/Models/Post.php
+++ Prakweb_A_203040036_Laravel/app/Models/Post.php
@@ -19,5 +19,11 @@ class Post extends Model
         return $this->belongsTo(Category::class);
     }
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    
+    }
+
 
 }
--- Prakweb_A_203040036_Laravel/resources/views/post.blade.php
+++ Prakweb_A_203040036_Laravel/resources/views/post.blade.php
@@ -4,11 +4,10 @@
     <article>
         <h1>{{ $post->title }}</h1>
 
-        <p>By. Caprio Sinaga in <a href="/categories/{{ $post->category->slug }}">
-            {{ $post->category->name }}</a></p>
+        <p>By. <a href="#" class="text-decoration-none">{{  $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
         
         {!! $post->body !!} 
     </article>
     
-    <a href="/blog">Back to Post</a>
+    <a href="/blog" class="d-block mt-3">Back to Post</a>
 @endsection
