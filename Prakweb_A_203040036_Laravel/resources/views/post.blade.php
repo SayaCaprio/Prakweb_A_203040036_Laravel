6cc5bea430c24eff873efcd52a36337be11667ee
caprio <203040036@mail.ac.id>
2022-10-26 23:42:25 +0700

    menyelesaikan video 8

--- Prakweb_A_203040036_Laravel/resources/views/post.blade.php
+++ Prakweb_A_203040036_Laravel/resources/views/post.blade.php
@@ -3,6 +3,9 @@
 @section('container')
     <article>
         <h1>{{ $post->title }}</h1>
+
+        <p>By. Caprio Sinaga in <a href="/categories/{{ $post->category->slug }}">
+            {{ $post->category->name }}</a></p>
         
         {!! $post->body !!} 
     </article>
